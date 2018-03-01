a766c09a14f27b5aee04d595027f2cd477df3a7a
Don Viszneki <don@codebad.com>
2018-02-28 19:18:35 -0800

    new main.py for training multiple networks

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import numpy as np
 import tensorflow as tf
+import math
 from model import Model
 from trainer import Trainer
 from audio import wavToStft, stftToWav
 
 input_filename = 'bar.wav'
-sample_rate, stft = wavToStft(input_filename, 8000 * 10)
-
-num_features = stft.shape[1]
+sample_rate, stft_real, stft_imag = wavToStft(
+  input_filename
+, 8000 * 10
+, return_imag = True
+)
+stft_both = np.concatenate(
+  (stft_real, stft_imag)
+, axis = 1
+)
+num_features = stft_both.shape[1]
 
 relu = tf.nn.relu
 
@@ -16,29 +25,29 @@ def reluM1(x):
 def addOneMorph(y):
   return y + 1
 
-for l1 in [8]:
-  for l2 in [8]:
-    for a1 in [tf.tanh, relu]:
-      for a2 in [tf.tanh, relu]:
-        for a3 in [tf.tanh, relu]:
-          for tries in range(4):
-            print(l1, l2, a1, a2, a3, tries)
-            tf.reset_default_graph()
-            target_steps = 120
-            model = Model(
-              num_features = num_features
-            , num_labels = num_features
-            , num_hidden = [l1, l2]
-            , activation = [a1, a2, a3]
-            , lr_decay_steps = target_steps
-            , auto_encoder = a3 is not relu
-            )
-            trainer = Trainer(
-              model
-            , x=stft
-            , y=stft
-            , steps_per_summary=40
-            , target_steps = target_steps
-            , y_morpher = addOneMorph if a3 is relu else None
-            )
-            trainer.train()
+for l1 in [8, 16, 32, 64]:
+  for a1 in [relu, tf.tanh, tf.sigmoid]:
+    for a2 in [tf.tanh, tf.sigmoid]:
+      for tries in range(4):
+        print(l1, a1, a2, tries)
+        tf.reset_default_graph()
+        target_steps = 1600000
+        model = Model(
+          num_features = num_features
+        , num_labels = num_features
+        , num_hidden = [l1]
+        , activation = [a1, a2]
+        , lr_decay_steps = target_steps
+        , auto_encoder = True
+        )
+        trainer = Trainer(
+          model
+        , x=stft_both
+        , y=stft_both
+        , steps_per_summary=200
+        , target_steps = target_steps
+        , target_cost = -math.inf
+        , optimizer = 'adam'
+        , cost = 'mse'
+        )
+        trainer.train()
